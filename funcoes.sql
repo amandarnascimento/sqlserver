93f8945dea512f2e433c3a19f067e546bf1bdb62
Amanda Nascimento <116469203+amandarnascimento@users.noreply.github.com>
2023-11-21 09:23:52 -0300

    Add files via upload
    Algumas das princiais funções básicas em T-SQL

---
+++ funcoes.sql
@@ -0,0 +1,24 @@
+
+
+/*************************************
+---   REMOVER ESPAÇOS -  exemplos
+*************************************/
+
+SELECT TRIM('   Olá   ') AS Resultado;
+SELECT LTRIM('   Olá   ') AS Resultado;
+
+/*************************************
+---   SUBSTITUIR PONTO POR VÍRGULA -  exemplos
+*************************************/
+
+REPLACE(PFUNC.SALARIO, '.', ',') AS 'REMUNERACAO_MENSAL',
+
+
+/*************************************
+---   COMPRIMENTO DE UM TEXTO -  exemplos
+*************************************/
+
+
+SELECT *
+FROM psecao
+WHERE LEN(codigo) = 33 AND SUBSTRING(codigo, 7, 4) = '0623';
